4874e412271723c3086a62ca0eccfbf38f7cfa79
Luis Angel Flores Del Castillo <floresdelcastilloluisangel@gmail.com>
2025-02-24 00:56:44 -0500

    aQuitando modal de maintenance

--- resources/js/components/Tailwind/Base.jsx
+++ resources/js/components/Tailwind/Base.jsx
@@ -4,22 +4,22 @@ import Footer from "./Footer";
 
 const Base = ({ children, footerLinks, session, socials, terms, showSlogan = true, showFooter = true, gradientStart = '#c4b8d3', gradientEnd = '#f1d7c1', menuGradientEnd = '#dbc8c9' }) => {
   
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    return () => {
-      document.body.style.overflow = 'unset';
-    };
-  }, []);
+  // useEffect(() => {
+  //   document.body.style.overflow = 'hidden';
+  //   return () => {
+  //     document.body.style.overflow = 'unset';
+  //   };
+  // }, []);
 
   return <section style={{
     backgroundImage: `linear-gradient(to right bottom, ${gradientStart}, ${gradientEnd})`
   }}>
-    <div className="fixed inset-0 backdrop-blur-sm bg-black/70 z-50 flex items-center justify-center min-h-screen w-full">
+    {/* <div className="fixed inset-0 backdrop-blur-sm bg-black/70 z-50 flex items-center justify-center min-h-screen w-full">
       <div className="text-center">
         <h2 className="text-4xl font-bold mb-4 text-white">En Mantenimiento</h2>
         <p className="text-gray-200 text-xl">Estamos realizando actualizaciones en el sistema. Por favor, vuelva más tarde.</p>
       </div>
-    </div>
+    </div> */}
     <Header showSlogan={showSlogan} gradientStart={gradientStart} menuGradientEnd={menuGradientEnd} session={session} />
     <main className="overflow-hidden min-h-[360px] relative">
       {children}
