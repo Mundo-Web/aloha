236d4689f67b1c7a26b3027b90becff5a234214b
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-06-27 19:49:06 -0500

    Agregando titulo de página

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -11,7 +11,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Página - {{ env('APP_NAME', 'AlohaPeru') }}</title>
+  <title>Página | {{ env('APP_NAME', 'AlohaPeru') }}</title>
 
   @isset($seoTitle)
     <meta name="title" content="{{ $seoTitle }}" />
